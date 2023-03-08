80e5408ddd26da4295cb5bbd533f77beb824252d
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-03-08 10:36:42 -0600

    Modificacion 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarEmpleadosComponent } from './listar-empleados/listar-empleados.component';
 
-const routes: Routes = [];
+//16.- Agregamos las rutas
+const routes: Routes = [
+  { path: 'empleados', component: ListarEmpleadosComponent },
+  { path: '', redirectTo: 'empleados', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prueba-Angular';
+  title = 'Sistema Gestion de Empleados'; //17.- Modificamos ya que es titulo a [app.component.html]
 }
